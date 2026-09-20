58cd922d55e407829085784f9b294c095775ac11
Geeta <patilgeeta2303@gmail.com>
2026-09-20 12:06:54 +0530

    Initial QuickKart setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-400">
+      <h1 className="text-4xl font-bold text-black">
+        QuickKart
+      </h1>
+    </div>
+  )
+}
+
+export default App
